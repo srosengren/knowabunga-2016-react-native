00b20f038ce22efa04bd7451d9a7d2529abd8afe
Sebastian Rosengrensebastian@rosengren.me <sebastian@rosengren.me>
2016-05-11 23:21:32 +0200

    Broadcast

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var wsServer = require('websocket').server;
+var uuid = require('node-uuid');
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
@@ -18,14 +19,20 @@ var socket = new wsServer({
     httpServer: server
 });
 
+var connections = {};
+
 socket.on('request', function(request) {
     var connection = request.accept(null, request.origin);
+    connection.id = uuid.v4();
+    connections[connection.id] = connection;
 
     connection.on('message', function(message) {
-        connection.sendUTF(JSON.stringify(message.utf8Data));
+        Object.keys(connections)
+            .map(k => connections[k])
+            .forEach(c => c.sendUTF(JSON.stringify(message.utf8Data)));
     });
 
     connection.on('close', function(connection) {
-        console.log('connection closed');
+        delete connections[connection.id];
     });
 });
